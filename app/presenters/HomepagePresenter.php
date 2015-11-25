4f9039f72820d091137c8da5dbae91cda5cbfe1a
Drahoslav Bednář <drahoslav@yo2.cz>
2015-11-25 12:30:32 +0100

    vraceni veci smazanych

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -1,10 +1,19 @@
 <?php
 
 namespace App\Presenters;
-
 use Nette;
-
 
 class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+	/** @var Nette\Database\Context */
+	private $database;
+	public function __construct(Nette\Database\Context $database)
+	{
+	   $this->database = $database;
+	}
+	public function renderDefault()
+	{
+		$this->template->test = $this->database->table('test')
+			->limit(5);
+	}
 }
