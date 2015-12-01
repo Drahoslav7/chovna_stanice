ff7908171706e02905fe103ffcdecc6736acd13c
David Balvín <l0g1n@seznam.cz>
2015-12-01 12:55:21 +0100

    GUI edit, návrat stavu Podle commitu c4ab75c4e20935a1ff1bea915d0700d0a03d8a15

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -2,6 +2,7 @@
 
 namespace App\Presenters;
 use Nette;
+use Nette\Application\UI\Form;
 
 class HomepagePresenter extends Nette\Application\UI\Presenter
 {
@@ -9,11 +10,23 @@ class HomepagePresenter extends Nette\Application\UI\Presenter
 	private $database;
 	public function __construct(Nette\Database\Context $database)
 	{
-	   $this->database = $database;
+		$this->database = $database;
+	}
+	public function actionDefault()
+	{
+
 	}
 	public function renderDefault()
 	{
 		$this->template->test = $this->database->table('test')
 			->limit(5);
+	}
+	public function createComponentSignInForm()
+	{
+		$form = new Form;
+		$form->addText('user')->setRequired();
+		$form->addPassword('password')->setRequired();
+		$form->addSubmit('send');
+		return $form;
 	}
 }
