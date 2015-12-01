9dfc0f3eead39b4a43591305cd8fb1e037b5bcbe
David Balvín <l0g1n@seznam.cz>
2015-12-01 12:49:51 +0100

    Error Authenticator

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -2,6 +2,7 @@
 
 namespace App\Presenters;
 use Nette;
+use Nette\Application\UI\Form;
 
 class HomepagePresenter extends Nette\Application\UI\Presenter
 {
@@ -9,11 +10,36 @@ class HomepagePresenter extends Nette\Application\UI\Presenter
 	private $database;
 	public function __construct(Nette\Database\Context $database)
 	{
-	   $this->database = $database;
+		$this->database = $database;
+	}
+	public function actionDefault()
+	{
+
 	}
 	public function renderDefault()
 	{
 		$this->template->test = $this->database->table('test')
 			->limit(5);
+	}
+	public function createComponentSignInForm()
+	{
+		$form = new Form;
+		$form->addText('user')->setRequired();
+		$form->addPassword('password')->setRequired();
+		$form->addSubmit('send');
+		$form->onSuccess[] = $this->signInFormSubmitted;
+		return $form;
+	}
+	public function signInFormSubmitted($form)
+	{
+		$values = $form->getValues();
+		$this->getUser()->setExpiration('+ 20 minutes', TRUE);
+		try {
+			$this->getUser()->login($values->user, $values->password);
+		} catch (Nette\Security\AuthenticationException $e) {
+			$form->addError($e->getMessage());
+			return;
+		}
+		$this->redirect('Homepage:');
 	}
 }
