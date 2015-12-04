8d86d61c27ac143fe283387b4b0a33cf276d033d
David Balvín <l0g1n@seznam.cz>
2015-12-04 03:41:06 +0100

    WIP GUI, nicer looking, new doglist

--- app/presenters/KeeperPresenter.php
+++ app/presenters/KeeperPresenter.php
@@ -3,13 +3,14 @@
 namespace App\Presenters;
 use Nette;
 use Nette\Application\UI\Form;
+use App\Model\DogModel;
 
 class KeeperPresenter extends BasePresenter
 {
 	public function actionDefault()
 	{
 		if ($this->template->isKeeper === true) {
-			$this->redirect("Keeper:dog");
+			$this->redirect("Keeper:doglist");
 		} else {
 			$this->redirect("Homepage:default");
 		}
@@ -19,6 +20,12 @@ class KeeperPresenter extends BasePresenter
 	public function renderDog()
 	{
 
+	}
+
+	public function renderDoglist()
+	{
+		$this->template->dogs = $this->mDog->getDogs();
+		$this->template->races = $this->mDog->getRaces();
 	}
 
 
