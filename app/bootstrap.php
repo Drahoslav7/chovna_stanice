041807304fdbd4321ac3abf44a96e8e8c7ae6c1b
Drahoslav Bednář <drahoslav@yo2.cz>
2015-12-04 12:24:15 +0100

    uprava getDog aby varel i nazvy plemen

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,6 +5,10 @@ require __DIR__ . '/../vendor/autoload.php';
 $configurator = new Nette\Configurator;
 
 //$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
+
+// $configurator->setDebugMode($configurator::NONE); // = FALSE
+
+
 $configurator->enableDebugger(__DIR__ . '/../log');
 
 $configurator->setTempDirectory(__DIR__ . '/../temp');
--- app/model/Dog.php
+++ app/model/Dog.php
@@ -10,7 +10,7 @@ class Dog extends BaseModel {
 
     public function getDogs()
     {
-        return $this->db->table('pes');
+        return $this->db->query('SELECT * FROM pes JOIN (SELECT ID as ID_2, Nazev AS Plemeno FROM pes_plemeno ) AS plemeno  WHERE plemeno.ID_2 = Plemeno_ID');
     }
 
 	public function getRaces()
